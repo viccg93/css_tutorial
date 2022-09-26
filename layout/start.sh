e14e47b61b84ed77132ea9eaed469da692c2af4c
victor <vic.cg93@gmail.com>
2022-09-26 01:05:33 -0500

    creacion de script, se agregan directorios y archivos iniciales

---
+++ layout/start.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+echo "obteniendo ruta para archivos iniciales"
+script_path=$(readlink -f "$0")
+root_path=$(dirname "$script_path")
+echo "ruta: $root_path"
+
+echo "generando directorios"
+
+capitulos=("introduccion" "normal_flow" "flexbox" "grids" "floats" "positioning" "multiple_column-layout" "responsive_design" 
+            "media_queries" "legacy_layout" "supporting_older_browsers" "assesments")
+
+for capitulo in ${capitulos[@]}
+do
+    if [ ! -d "$root_path/$capitulo" ]; then
+        mkdir "$root_path/$capitulo"
+        touch "$root_path/$capitulo/$capitulo.html"
+        touch  "$root_path/$capitulo/$capitulo.css"
+        echo "$root_path/$capitulo generado"
+    else
+        echo "$root_path/$capitulo ya existe, no se generan archivos en esta ruta"
+    fi
+done
+
+echo "generando README"
+touch "$root_path/README.md"
+echo "ejecucion finalizada"
